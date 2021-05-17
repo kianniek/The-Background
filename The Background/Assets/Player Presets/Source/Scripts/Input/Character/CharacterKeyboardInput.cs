367729f3b2b26738b1143d17ed550c16db52490b
kianniek <56929026+kianniek@users.noreply.github.com>
2021-05-17 14:05:36 +0200

    update3

---
+++ The Background/Assets/Player Presets/Source/Scripts/Input/Character/CharacterKeyboardInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CMF
+{
+	//This character movement input class is an example of how to get input from a keyboard to control the character;
+    public class CharacterKeyboardInput : CharacterInput
+    {
+		public string horizontalInputAxis = "Horizontal";
+		public string verticalInputAxis = "Vertical";
+		public KeyCode jumpKey = KeyCode.Space;
+		public KeyCode runningKey = KeyCode.LeftShift;
+		public KeyCode crouchKey = KeyCode.LeftControl;
+
+		//If this is enabled, Unity's internal input smoothing is bypassed;
+		public bool useRawInput = true;
+
+        public override float GetHorizontalMovementInput()
+		{
+			if(useRawInput)
+				return Input.GetAxisRaw(horizontalInputAxis);
+			else
+				return Input.GetAxis(horizontalInputAxis);
+		}
+
+		public override float GetVerticalMovementInput()
+		{
+			if(useRawInput)
+				return Input.GetAxisRaw(verticalInputAxis);
+			else
+				return Input.GetAxis(verticalInputAxis);
+		}
+		public override bool IsRunningKeyPressed()
+		{
+			return Input.GetKey(runningKey);
+		}
+		public override bool IsRunningKeyLetGo()
+		{
+			return Input.GetKeyUp(runningKey);
+		}
+
+		public override bool IsJumpKeyPressed()
+		{
+			return Input.GetKey(jumpKey);
+		}
+
+	}
+}
